cc368e9d604ba04122292d3daa12194ed6284694
Ron Tor Das <ronaldtd85@gmail.com>
2019-04-07 18:35:15 +0800

    first commit

--- ML A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/polynomial_regression.py
+++ ML A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/polynomial_regression.py
@@ -4,7 +4,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -25,11 +24,6 @@ X_test = sc_X.transform(X_test)"""
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
-plt.plot(X, y)
-plt.plot(X, lin_reg.predict(X))
-dataset2 = pd.DataFrame(np.append(X, y.reshape(len(y), 1), axis=1))
-dataset2.columns = ['Level', 'Salary']
-sns.lmplot('Level', 'Salary', data=dataset2)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
@@ -38,8 +32,6 @@ X_poly = poly_reg.fit_transform(X)
 poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
-
-X_poly
 
 # Visualising the Linear Regression results
 plt.scatter(X, y, color = 'red')
